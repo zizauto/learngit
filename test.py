76667345e7e979609ee142af30668b3bea929565
zhangchao055 <zhangchao055@163.com>
2018-03-28 10:37:11 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Name:
+# test.py
+# Program:
+# This program shows "Hello python3!" in your screen.
+# History:
+# 2018/03/28	zhangchao055	First release
+
+print("Hello python3!")
